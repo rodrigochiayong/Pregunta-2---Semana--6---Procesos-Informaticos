e3c0b566420eb9c240cb21ec41016c1b17af528d
rodrigochiayong <rodrigochiayong@gmail.com>
2024-09-22 21:11:25 -0300

    archivos

---
+++ Pregunta2/js/searchIndex.js
@@ -0,0 +1,49 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\ComercioElectronico",
+            "name": "ComercioElectronico",
+            "summary": "Clase\u0020ComercioElectronico",
+            "url": "classes/ComercioElectronico.html"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor\u0020de\u0020la\u0020clase.",
+            "url": "classes/ComercioElectronico.html#method___construct"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003AagregarProducto\u0028\u0029",
+            "name": "agregarProducto",
+            "summary": "Agrega\u0020un\u0020producto\u0020al\u0020inventario.",
+            "url": "classes/ComercioElectronico.html#method_agregarProducto"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003AagregarAlCarrito\u0028\u0029",
+            "name": "agregarAlCarrito",
+            "summary": "Agrega\u0020un\u0020producto\u0020al\u0020carrito\u0020de\u0020compras.",
+            "url": "classes/ComercioElectronico.html#method_agregarAlCarrito"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003ArealizarPago\u0028\u0029",
+            "name": "realizarPago",
+            "summary": "\u002A\u0020Calcula\u0020y\u0020muestra\u0020el\u0020total\u0020a\u0020pagar\u0020por\u0020los\u0020productos\u0020en\u0020el\u0020carrito.",
+            "url": "classes/ComercioElectronico.html#method_realizarPago"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003AvaciarCarrito\u0028\u0029",
+            "name": "vaciarCarrito",
+            "summary": "Vac\u00EDa\u0020el\u0020carrito\u0020de\u0020compras.",
+            "url": "classes/ComercioElectronico.html#method_vaciarCarrito"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003A\u0024productos",
+            "name": "productos",
+            "summary": "",
+            "url": "classes/ComercioElectronico.html#property_productos"
+        },                {
+            "fqsen": "\\ComercioElectronico\u003A\u003A\u0024carrito",
+            "name": "carrito",
+            "summary": "",
+            "url": "classes/ComercioElectronico.html#property_carrito"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
